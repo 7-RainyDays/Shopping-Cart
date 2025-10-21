7e9c433beff0f44517a2bacfc457c77d482d3682
Stewie1 <xionone@web.de>
2025-10-21 13:56:04 +0200

    dynamically display shopping cart items at checkout

--- README.md
+++ README.md
@@ -3,26 +3,3 @@ Shopping Cart Project
 This Project in part of the ODIN project. For more visit: https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart
 
 https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart
-
-PLAN
-
-Think about the component and the folder structure. How could you set up your application? Which components or functionalities do you need? It’s a good idea to note this down somewhere you can easily get to and refer back and add to it to keep track.
-
-pages:
--homepage
--shop-page
--checkout with overview
-
-Component:
--Navbar
--Product-Grid
--Product-Card
--shopping-Card
-
-Cart:
-//Cart Symbol nur für Anzahl der Items
-//Nutzer Informieren wenn item reingepackt
-//Zusammenfassung aller Items irgendwo
-
-TODO:
--add rating and count to product cards
--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,18 +1,34 @@
 import Navbar from "../Navbar/Navbar";
+import classes from "./Checkout.module.css";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import { useState, useMemo } from "react";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 export default function Checkout({ cartItems, setCartItems }) {
+  const rowData = useMemo(() => {
+    return cartItems.map((item) => ({
+      title: item.title,
+      price: item.price,
+      quantity: item.quantity,
+    }));
+  }, [cartItems]);
+
+  const [colDefs, setColDefs] = useState([
+    { field: "title" },
+    { field: "price" },
+    { field: "quantity" },
+  ]);
+
   return (
     <>
       <Navbar />
-      <div>
+      <div className={classes.checkoutTable}>
         <h2>Checkout</h2>
-        {cartItems.map((item) => (
-          <div key={item.id}>
-            <p>{item.title}</p>
-            <p>{item.price}</p>
-            <p>{item.quantity}</p>
-          </div>
-        ))}
+        <div style={{ height: 500 }}>
+          <AgGridReact rowData={rowData} columnDefs={colDefs} />
+        </div>
       </div>
     </>
   );
