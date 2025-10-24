e90d0c083630f74460d49bc2442d40a6205bd5a4
Stewie1 <xionone@web.de>
2025-10-24 10:46:32 +0200

    test checkout component

--- src/tests/Checkout.test.jsx
+++ src/tests/Checkout.test.jsx
@@ -2,9 +2,28 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Checkout from "../components/checkout/Checkout.jsx";
-import ProductCard from "../components/store/ProductCard.jsx";
-import { testdata as items } from "./testdata.js";
+import testdata from "./testdata.js";
 
-describe("shopping cart functionality and display", () => {
-  return true;
+describe("Checkout component", () => {
+  it("renders an item correctly in the table", () => {
+    const cartItems = [
+      {
+        ...testdata[0],
+        quantity: 2,
+      },
+    ];
+
+    render(<Checkout cartItems={cartItems} setCartItems={() => {}} />);
+
+    expect(
+      screen.getByText("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops")
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("109.95")).toBeInTheDocument();
+
+    expect(screen.getByText("219.9")).toBeInTheDocument();
+
+    const rows = screen.getAllByRole("row");
+    expect(rows.length).toBe(2);
+  });
 });
